d8c327326a549d7860e29835269be55a8db142ad
lonestar299 <gordonallanarisasira@gmail.com>
2026-04-14 21:19:00 +0300

    variables and datatypes

---
+++ intro.js
@@ -0,0 +1,40 @@
+let name = "mike"
+console.log(name)
+let age=25
+console.log(age)
+const pi=22/7
+console.log (pi)
+//datatypes
+//string;its a collection of characters(we put it inside "")
+let city="kampala"
+console.log(city)
+let school="kampala parents"
+console.log(school)
+//number;its any numerical value.it can contain decimals or not
+let weight=56
+console.log(weight)
+let height=5.8
+console.log(height)
+//boolean;this means true or false
+let isadult=true
+console.log(isadult)
+let isstudent=false
+console.log(isstudent)
+//array;its a collection/list of items
+let students=["gordon","allan","stacey"]
+console.log(students)
+let marks=[20,30,40,50,60]
+console.log(marks)
+//object;it contains key value pairs
+let user={
+    name:"anthony",
+    age:56,
+    email:"user@gmail.com"
+}
+console.log(user)
+let student={
+    name:"bernard",
+    course:"BDS",
+    year:2026
+}
+console.log(student)
